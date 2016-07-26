80d2510953a0aa1dc321051eaf2710c1120a6c44
luxxsys <luxxsys@163.com>
2016-07-26 23:29:10 +0800

    fix auto light bug

--- controller.py
+++ controller.py
@@ -14,7 +14,7 @@ distancefile='mr6_distancefile'
 
 aircstate_old=0
 lightstate_old=0
-IRP_TTL=IRP_TTL_init=20
+IRP_TTL=IRP_TTL_init=30
 
 def setAirc(serial, state, times=1):
 	global aircstate_old
@@ -27,12 +27,10 @@ def setAirc(serial, state, times=1):
 			for i in range(0,times):
 				serial.write(msgon)
 				time.sleep(1.5)
-			fileRW(aircfile,'w',state)
 		if(0==state):
 			for i in range(0,times):
 				serial.write(msgoff)
 				time.sleep(1.5)
-			fileRW(aircfile,'w',state)
 		aircstate_old=state
 
 def setLight(serial, state, times=1):
@@ -43,17 +41,15 @@ def setLight(serial, state, times=1):
 	msgoff=b'00001001:00DD:01:0001'
 	if(state!=lightstate_old):
 		if(1==state):
-			print('hereon')
+			#print('hereon')
 			for i in range(0,times):
 				serial.write(msgon)
-				time.sleep(1.5)
-			fileRW(lightfile,'w',state)
+				time.sleep(0.5)
 		if(0==state):
-			print('hereoff')
+			#print('hereoff')
 			for i in range(0,times):
 				serial.write(msgoff)
-				time.sleep(1.5)
-			fileRW(lightfile,'w',state)
+				time.sleep(0.5)
 		lightstate_old=state
 
 mys0=serial.Serial('/dev/serial0', 9600, timeout=0.5)
@@ -69,25 +65,28 @@ while True:
 		if(aircmstate<0):
 			distance=getDistance()
 			if(distance*1000<500):
-				setAirc(mys0,1,3)
+				fileRW(aircfile,'w',1)
 			else:
-				setAirc(mys0,0,3)
+				fileRW(aircfile,'w',0)
+			setAirc(mys0,aircstate,2)
 		else:
-			setAirc(mys0,aircmstate, 3)
+			setAirc(mys0,aircmstate,2)
 
 		if(lightmstate>=0):
 			setLight(mys0,lightmstate,3)
+			IRP_TTL=0
+		elif(lightmstate<0):
+			setLight(mys0,lightstate,3)
 
 
 		#温湿度、光线参数提取
 		#readline()接收一行数据直到遇到\n，净化、并将bytes解析为str
-		recv=mys0.readline().strip()
-		#print(recv)
+		recv=(mys0.readline().strip()).decode('ascii')
+		print(recv)
 		
 		#data example: "HUMI52;TEMP28;LUX1021;"
 		#LUX有光为30，无光为1000，浮动两字节，自有长度匹配时才进行处理，否则为废串
 		if(22==len(recv) or 20==len(recv)):
-			recv=recv.decode('ascii') #收到为bytes，转为str
 			load=recv.split(';')
 
 			humidata=re.search(r'HUMI(\d+)', load[0])
@@ -100,18 +99,21 @@ while True:
 			fileRW(luxfile, 'w', luxdata.group(1))
 			#print(luxdata.group(1))
 
-		elif('IRPACTIVE'==recv.decode('ascii') and lightmstate<0):
+		elif('IRPACTIVE'==recv and lightmstate<0):
 			if(lux>500):
-				setLight(mys0,1,3)
+				#print(lux)
+				fileRW(lightfile,'w',1)
+			elif(lux<500):
+				continue
 			IRP_TTL=IRP_TTL_init
 			print(IRP_TTL)
 		else:
 			IRP_TTL-=1
 			print(IRP_TTL)
 			if(0==IRP_TTL):
-				setLight(mys0,0,3)
+				fileRW(lightfile,'w',0)
 			elif(IRP_TTL<0):
 				IRP_TTL=0
 	except Exception as e:
 		print(e)
-#		continue
+		continue
